6eec8dcb05fc69f5827cec203eb3469e70e69099
Roqez <roger.b96@gmail.com>
2023-01-11 14:51:55 +0200

    Toinen tehtävä

---
+++ Moduuli 1/Tehtava2.5.py
@@ -0,0 +1,9 @@
+leiviska = float((input)("Syötä leiviskät "))
+naula = float((input("Syötä naulat ")))
+luoti = float((input)("Syötä luodit "))
+naulat = (leiviska*20)+naula
+luodit = (naulat*32)+luoti
+paino = luodit*13.3
+kilogrammat = paino//1000
+grammat = paino%1000
+print(f"Massa nyky mittojen mukaan: {kilogrammat:2.0f} kiloa ja "f"{grammat:2.2f} grammaa.")
