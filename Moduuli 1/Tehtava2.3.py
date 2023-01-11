0f4a5373ff0438cd3c5deb4a018088d40f5fc60c
Roqez <roger.b96@gmail.com>
2023-01-11 14:27:53 +0200

    Toinen tehtävä

---
+++ Moduuli 1/Tehtava2.3.py
@@ -0,0 +1,5 @@
+kanta = float(input("Syötä suorakulmion kanta"))
+korkeus = float(input("Syötä suorakulmion kanta"))
+ala = kanta * korkeus
+piiri = kanta * 2 + korkeus * 2
+print("Suorakulmion pinta-ala on "+str(ala)+" ja piiri on "+str(piiri))
---
+++ Moduuli 1/Tehtava2.4.py
@@ -0,0 +1,7 @@
+eka = float(input("Syötä ensimmäinen luku"))
+toka = float(input("Syötä toinen luku"))
+kolmas = float(input("Syötä kolmas luku"))
+summa = eka+toka+kolmas
+tulo = eka*toka*kolmas
+keskiarvo = (eka+toka+kolmas)/3
+print("Lukujen summa on "+str(summa)+" tulo on "+str(tulo)+" keskiarvo on "+str(keskiarvo))
