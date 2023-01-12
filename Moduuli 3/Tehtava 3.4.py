b6324f9e28c7e0c5c48ef310cc6289c025fbf63b
Roqez <roger.b96@gmail.com>
2023-01-12 11:35:13 +0200

    Moduuli 3

---
+++ Moduuli 3/Tehtava 3.4.py
@@ -0,0 +1,6 @@
+vuosi = int(input("Syötä vuosiluku niin kerron onko se karkausvuosi"))
+
+if vuosi%100 == 0 and vuosi%400 == 0:
+    print("Vuosi on karkausvuosi")
+elif vuosi%4 == 0 and vuosi%100 != 0:
+    print("Vuosi on karkausvuosi")
