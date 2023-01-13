51a3eb87d24fd662351f680c22e2e6cea55cb2f4
Roqez <roger.b96@gmail.com>
2023-01-13 11:58:11 +0200

    Moduuli 3

--- Moduuli 3/Tehtava 3.4.py
+++ Moduuli 3/Tehtava 3.4.py
@@ -4,3 +4,5 @@ if vuosi%100 == 0 and vuosi%400 == 0:
     print("Vuosi on karkausvuosi")
 elif vuosi%4 == 0 and vuosi%100 != 0:
     print("Vuosi on karkausvuosi")
+else:
+    print("Vuosi ei ole karkausvuosi")
