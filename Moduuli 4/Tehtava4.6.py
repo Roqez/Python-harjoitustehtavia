0b03186fdea09ab4b7c87ffe8a138a7222ea93ad
Roqez <roger.b96@gmail.com>
2023-01-17 15:51:08 +0200

    Moduuli 4

--- Moduuli 4/Tehtava4.6.py
+++ Moduuli 4/Tehtava4.6.py
@@ -5,14 +5,9 @@ toistot = N
 while toistot > 0:
     x = random.uniform(-1, 1)
     y = random.uniform(-1, 1)
-    print("X arvo on"+ str(x))
-    print("Y arvo on" + str(y))
     laskutoimitus = x**2+y**2
-    print(laskutoimitus)
     toistot = toistot - 1
-    print(N)
     if float(laskutoimitus) < 1:
         n = n + 1
 pii = 4 * n/N
-print("Osumat ympyrään "+ str(n))
 print("Tässä on piin likiarvo "+ str(pii))
