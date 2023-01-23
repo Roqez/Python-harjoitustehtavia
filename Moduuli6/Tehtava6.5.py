e1c1a124c5b354c0ce043ac6410e5bcd4a7bce3b
Roqez <roger.b96@gmail.com>
2023-01-23 13:27:50 +0200

    Moduuli 6

---
+++ Moduuli6/Tehtava6.5.py
@@ -0,0 +1,11 @@
+def summaus(luvut):
+    for i in luvut:
+        if i%2 != 0:
+            uusi_lista.append(i)
+    return uusi_lista
+
+
+uusi_lista = []
+luvut = [1,2,5,8,9,101,550,333]
+print(luvut)
+print(summaus(luvut))
