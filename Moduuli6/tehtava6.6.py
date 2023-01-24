bfd29f4db3a62f2534fa53e7bc8724b42426e1a3
Roqez <roger.b96@gmail.com>
2023-01-24 12:52:04 +0200

    Moduuli 6

--- Moduuli6/tehtava6.6.py
+++ Moduuli6/tehtava6.6.py
@@ -10,14 +10,10 @@ def main():
     halkaisija = int(input("Syötä ensimmäisen pizzan halkaisija senttimetreinä "))
     hinta = int(input("Syötä ensimmäisen pizzan hinta euroina "))
     ensimmainen_pizza = pizza(halkaisija,hinta)
-    print (ensimmainen_pizza)
     halkaisija = int(input("Syötä toisen pizzan halkaisija senttimetreinä "))
     hinta = int(input("Syötä toisen pizzan hinta euroina "))
-    toinen_pizza = int(pizza(halkaisija,hinta))
-    print(toinen_pizza)
-    if ensimmainen_pizza > toinen_pizza:
-        print(ensimmainen_pizza)
-        print(toinen_pizza)
+    toinen_pizza = pizza(halkaisija,hinta)
+    if ensimmainen_pizza < toinen_pizza:
         print("Toinen pizza antaa paremman vastineen rahalle")
     else:
         print("Ensimmäinen pizza antaa paremman vastineen rahalle")
