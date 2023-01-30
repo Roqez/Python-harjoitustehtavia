21561c928d142c43f6d622ad87cd142493ddfa4f
Roqez <roger.b96@gmail.com>
2023-01-30 20:02:23 +0200

    Moduuli 8

--- Moduuli 8/Tehtava8.3.py
+++ Moduuli 8/Tehtava8.3.py
@@ -1,9 +1,6 @@
 import mysql.connector
 
 from geopy import distance
-#newport_ri = (41.49008, -71.312796)
-#cleveland_oh = (41.499498, -81.695391)
-#print(distance.distance(newport_ri, cleveland_oh).miles)
 
 yhteys = mysql.connector.connect(
          host='127.0.0.1',
