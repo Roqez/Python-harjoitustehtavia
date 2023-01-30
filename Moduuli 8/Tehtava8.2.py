db4132f37ebd1e4198a6f75a572048fbcf920d1c
Roqez <roger.b96@gmail.com>
2023-01-30 18:48:00 +0200

    Moduuli 8

---
+++ Moduuli 8/Tehtava8.2.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+def haekentat(maakoodi):
+    sql = "SELECT TYPE, COUNT(iso_country) FROM airport WHERE iso_country ='"+maakoodi+ "' GROUP BY TYPE;"
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            print(f"Lentokentän tyyppi on {rivi[0]} ja niiden lukumäärä on {rivi[1]}")
+
+
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='juuri1234',
+         autocommit=True
+         )
+
+
+def main():
+    maakoodi = input("Anna maakoodi: ")
+    haekentat(maakoodi)
+
+main()
