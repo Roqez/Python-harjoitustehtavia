6d9ac829cad7a3aeb0b98d7951d11e62ff6c9418
Roqez <roger.b96@gmail.com>
2023-03-15 12:47:19 +0200

    Moduuli 10

--- Moduuli 10/Tehtävä 10.1.py
+++ Moduuli 10/Tehtävä 10.1.py
@@ -2,22 +2,29 @@ class Hissi:
     def __init__(self,alinkerros,ylinkerros):
         self.alinkerros = alinkerros
         self.ylinkerros = ylinkerros
-        self.kerrosnyt = 0
-
+        self.kerrosnyt = alinkerros
     def kerros_ylös(self,kerrosnyt,ylinkerros):
         if kerrosnyt + 1 <= ylinkerros:
             self.kerrosnyt = kerrosnyt + 1
         else:
             print("Olet jo ylimmässä kerroksessa")
+        print("Olet nyt "+str(self.kerrosnyt)+" kerroksessa")
     def kerros_alas(self,kerrosnyt,alinkerros):
         if kerrosnyt - 1 >= alinkerros:
             self.kerrosnyt = kerrosnyt - 1
         else:
             print("Olet jo alimmassa kerroksessa")
-
-    def siirry_kerrokseen(self,alinkerros,ylinkerros,haluttukerros):
-        if haluttukerros >= alinkerros and haluttukerros <= ylinkerros:
-            self.kerrosnyt = haluttukerros
+        print("Olet nyt "+str(self.kerrosnyt)+" kerroksessa")
+    def siirry_kerrokseen(self,haluttukerros):
+        if haluttukerros >= self.alinkerros and haluttukerros <= self.ylinkerros:
+            if haluttukerros > self.kerrosnyt:
+                while self.kerrosnyt < haluttukerros:
+                    h.kerros_ylös(self.kerrosnyt,self.ylinkerros)
+            else:
+                while self.kerrosnyt > haluttukerros:
+                    h.kerros_alas(self.kerrosnyt,self.alinkerros)
         else:
             print("Virheellinen kerros")
-
+h = Hissi (0,5)
+h.siirry_kerrokseen(5)
+h.siirry_kerrokseen(0)
