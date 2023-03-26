76155e5d656a47de6198b42cd8c7014dd71c2870
Roqez <roger.b96@gmail.com>
2023-03-26 23:08:25 +0300

    Moduuli 10

---
+++ Moduuli12/Tehtävä 12.2.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+paikkakunta = input("Syötä paikkakunta niin haen sen säätilan")
+
+pyyntö = "https://api.openweathermap.org/data/2.5/weather?q="+paikkakunta+"&appid=f8040d0df22456d3e3472beeba1ca91b"
+pyyntö2 = "https://api.openweathermap.org/data/2.5/weather?q="+paikkakunta+"&units=metric&appid=f8040d0df22456d3e3472beeba1ca91b"
+try:
+    vastaus = requests.get(pyyntö)
+    if vastaus.status_code==200:
+        json_vastaus = vastaus.json()
+        for i in json_vastaus["weather"]:
+            print("Säätila on: " + i["main"])
+        kelvinit = (json_vastaus["main"]["temp"])
+        vastaus2 = requests.get(pyyntö2)
+        json_vastaus2 = vastaus2.json()
+        celsius_asteet = (json_vastaus2["main"]["temp"])
+        print("Säätila on: " + i["main"]+" ja asteet kelvineinä on: "+str(kelvinit)+" sekä asteet celsiuksina on: "+str(celsius_asteet))
+except requests.exceptions.RequestException as e:
+    print("Hakua ei voitu suorittaa")
